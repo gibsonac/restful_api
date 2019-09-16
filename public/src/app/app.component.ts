84208ee448e886ca4fa41e90eed163914f3c20f4
Adam Gibson <gibsoncadam@gmail.com>
2019-09-16 15:55:32 -0700

    added changes

--- public/src/app/app.component.ts
+++ public/src/app/app.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpService } from './http.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
-  constructor(private _httpService: HttpService){}
+  tasks: any = [];
+  id: string = '5d8009d658724b74cf47f1ad';
+  task: any = {};
+
+  constructor(private _httpService: HttpService) {
+  }
+  ngOnInit() {
+    this.getTasksFromService();
+    this.getTaskFromService(this.id);
+  }
+  getTasksFromService() {
+    let observable = this._httpService.getTasks();
+    observable.subscribe(data => {
+      console.log("we have the tasks!", data);
+      this.tasks = data;
+    })
+
+  }
+  getTaskFromService(id) {
+    let tempObservable = this._httpService.getTask(id)
+    tempObservable.subscribe(data => {
+      console.log("here is the id deets:", data)
+      this.task = data;
+    });
+  }
 }
--- public/src/app/http.service.ts
+++ public/src/app/http.service.ts
@@ -7,20 +7,20 @@ import { HttpClient } from '@angular/common/http';
 export class HttpService {
 
   constructor(private _http: HttpClient) {
-    this.getTasks();
-    this.getTask('5d7d6196fef9cf5e47e4f3e0');
-    this.deleteTask('5d7d6196fef9cf5e47e4f3e0');
-    this.getTasks();
+    // this.getTasks();
+    // this.getTask('5d7d6196fef9cf5e47e4f3e0');
+    // this.deleteTask('5d7d6196fef9cf5e47e4f3e0');
+    // this.getTasks();
   }
   getTasks() {
     // our http response is an Observable, store it in a variable
-    let tempObservable = this._http.get('/tasks');
+    return this._http.get('/tasks');
     // subscribe to the Observable and provide the code we would like to do with our data from the response
-    tempObservable.subscribe(data => console.log("Got our tasks!", data));
+    // tempObservable.subscribe(data => console.log("Got our tasks!", data));
   }
   getTask(id) {
-    let tempObservable = this._http.get(`/tasks/${id}`)
-    tempObservable.subscribe(data => console.log("here is the id deets:", data));
+    return this._http.get(`/tasks/${id}`)
+    // tempObservable.subscribe(data => console.log("here is the id deets:", data));
   }
   deleteTask(id) {
     let tempObservable = this._http.delete(`/tasks/${id}`)
